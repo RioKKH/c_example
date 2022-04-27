bad7f96472fbe14205219202370451f38e1648e9
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2022-04-27 21:55:52 +0900

    new file:   code_5_1_print_my_own_process_ID.c 	new file:   ex_5_3_get_my_PID.c

---
+++ code_5_1_print_my_own_process_ID.c
@@ -0,0 +1,11 @@
+#include <stdio.h>			// printf
+#include <stdlib.h>			// exit
+#include <sys/types.h>		// getpid
+#include <unistd.h>			// getpid
+
+int main(void)
+{
+	printf("pid = %d\n", getpid());
+
+	exit(0);
+}
